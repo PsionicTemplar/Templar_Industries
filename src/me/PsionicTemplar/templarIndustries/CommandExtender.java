fd8c79c82a5639f4082930adc3c1f82de2b00374
Nicholas Braniff <nicholas.braniff@live.com>
2017-12-19 00:56:27 -0500

    Added in basic command extender and a basic test command that is to be removed later

--- src/me/PsionicTemplar/templarIndustries/CommandExtender.java
+++ src/me/PsionicTemplar/templarIndustries/CommandExtender.java
@@ -1,5 +1,24 @@
 package me.PsionicTemplar.templarIndustries;
 
-public class CommandExtender {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandExtender implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command base, String alias, String[] args) {
+		Player p = null;
+		if ((sender instanceof Player)) {
+			p = (Player) sender;
+		}
+		switch(alias.toString()){
+		case "test":
+			p.sendMessage("Yay!");
+			return true;
+		}
+		return false;
+	}
 
 }
