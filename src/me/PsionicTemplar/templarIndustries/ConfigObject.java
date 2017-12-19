2ec5930cdb6e91c04cb19e890ab6e5fadd84489b
Nicholas Braniff <nicholas.braniff@live.com>
2017-12-19 01:10:00 -0500

    added basics for ConfigObject

--- src/me/PsionicTemplar/templarIndustries/ConfigObject.java
+++ src/me/PsionicTemplar/templarIndustries/ConfigObject.java
@@ -1,5 +1,68 @@
 package me.PsionicTemplar.templarIndustries;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 public class ConfigObject {
+
+	private boolean mainConfig;
+	
+	private FileConfiguration config;
+	private File file;
+	private String filePath;
+	private String fileName;
+
+	public ConfigObject(FileConfiguration config) {
+		this.mainConfig = true;
+		this.config = config;
+	}
+
+	public ConfigObject(String path, String fileName, HashMap<String, Type> defaults) {
+		this.mainConfig = false;
+		this.filePath = path;
+		this.fileName = fileName;
+		try {
+			file = new File(this.filePath, this.fileName);
+			if (!Start.getPlugin().getDataFolder().exists()) {
+				Start.getPlugin().getDataFolder().mkdirs();
+			}
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			config = new YamlConfiguration();
+			config.load(file);
+
+			for(String s : defaults.keySet()){
+				config.addDefault(s, defaults.get(s));
+			}
+			config.options().copyDefaults(true);
+
+			write();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void write() {
+		if (this.mainConfig) {
+			Start.getPlugin().saveConfig();
+		} else {
+			try {
+				Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.file)));
+				out.write(config.saveToString());
+				out.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
