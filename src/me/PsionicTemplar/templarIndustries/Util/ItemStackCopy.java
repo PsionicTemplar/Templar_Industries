641c142d826b97f675f63640b525dfe1ae392b1b
PsionicTemplar <nicholas.braniff@live.com>
2018-12-08 18:10:58 -0500

    Added documentation to ItemStackCopy util. Added an additional method to ItemStackCopy to allow you to add names and lore to a tool.

--- src/me/PsionicTemplar/templarIndustries/Util/ItemStackCopy.java
+++ src/me/PsionicTemplar/templarIndustries/Util/ItemStackCopy.java
@@ -9,6 +9,15 @@ import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemStackCopy {
+
+	/**
+	 * Copy's an ItemStack and sets the amount to 1
+	 * 
+	 * @author Nicholas Braniff
+	 * @param copyThis
+	 * @return
+	 */
+
 	public static ItemStack getItemStackCopy(ItemStack copyThis) {
 		ItemStack i = new ItemStack(copyThis.getType());
 		i.setItemMeta(copyThis.getItemMeta());
@@ -19,10 +28,19 @@ public class ItemStackCopy {
 		}
 		return i;
 	}
-	
+
+	/**
+	 * Copy's an ItemStack with a specified amount
+	 * 
+	 * @author Nicholas Braniff
+	 * @param copyThis
+	 * @param amount
+	 * @return
+	 */
+
 	public static ItemStack getItemStackCopy(ItemStack copyThis, int amount) {
 		ItemStack i = new ItemStack(copyThis.getType(), amount);
-		
+
 		i.setItemMeta(copyThis.getItemMeta());
 		if (!copyThis.getEnchantments().isEmpty()) {
 			for (Enchantment e : copyThis.getEnchantments().keySet()) {
@@ -31,10 +49,21 @@ public class ItemStackCopy {
 		}
 		return i;
 	}
-	
+
+	/**
+	 * Copy's an ItemStack and allows you to add both a name, lore, and amount
+	 * 
+	 * @author Nicholas Braniff
+	 * @param copyThis
+	 * @param amount
+	 * @param name
+	 * @param lore
+	 * @return
+	 */
+
 	public static ItemStack getItemStackCopy(ItemStack copyThis, int amount, String name, List<String> lore) {
 		ItemStack i = new ItemStack(copyThis.getType(), amount);
-		
+
 		i.setItemMeta(copyThis.getItemMeta());
 		ItemMeta im = i.getItemMeta();
 		im.setDisplayName(name);
@@ -48,14 +77,61 @@ public class ItemStackCopy {
 		}
 		return i;
 	}
-	
-	public static ItemStack getItemStackCopy(ItemStack copyThis, int amount, short damage, Map<Enchantment, Integer> enchants) {
+
+	/**
+	 * Copy's an ItemStack. Specify the amount, damage, and enchantments. Useful for
+	 * tools.
+	 * 
+	 * @author Nicholas Braniff
+	 * @param copyThis
+	 * @param amount
+	 * @param damage
+	 * @param enchants
+	 * @return
+	 */
+
+	public static ItemStack getItemStackCopy(ItemStack copyThis, int amount, short damage,
+			Map<Enchantment, Integer> enchants) {
 		ItemStack i = new ItemStack(copyThis.getType(), amount);
-		
+
 		i.setItemMeta(copyThis.getItemMeta());
 		Damageable dam = (Damageable) i.getItemMeta();
 		dam.setDamage(damage);
 		i.setItemMeta((ItemMeta) dam);
+		if (enchants != null && !enchants.isEmpty()) {
+			for (Enchantment e : enchants.keySet()) {
+				i.addUnsafeEnchantment(e, enchants.get(e));
+			}
+		}
+		return i;
+	}
+
+	/**
+	 * Copy's an ItemStack. Specify the amount, damage, enchantments, name, and
+	 * lore. Useful for custom tools.
+	 * 
+	 * @author Nicholas Braniff
+	 * @param copyThis
+	 * @param amount
+	 * @param damage
+	 * @param enchants
+	 * @param name
+	 * @param lore
+	 * @return
+	 */
+
+	public static ItemStack getItemStackCopy(ItemStack copyThis, int amount, short damage,
+			Map<Enchantment, Integer> enchants, String name, List<String> lore) {
+		ItemStack i = new ItemStack(copyThis.getType(), amount);
+
+		i.setItemMeta(copyThis.getItemMeta());
+		Damageable dam = (Damageable) i.getItemMeta();
+		dam.setDamage(damage);
+		i.setItemMeta((ItemMeta) dam);
+		ItemMeta im = i.getItemMeta();
+		im.setDisplayName(name);
+		im.setLore(lore);
+		i.setItemMeta(im);
 		if (enchants != null && !enchants.isEmpty()) {
 			for (Enchantment e : enchants.keySet()) {
 				i.addUnsafeEnchantment(e, enchants.get(e));
