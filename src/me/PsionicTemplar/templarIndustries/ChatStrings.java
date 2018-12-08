6352b3e7a0d54a6e8e78baea0fc3db790c7d3338
PsionicTemplar <nicholas.braniff@live.com>
2018-12-08 16:10:10 -0500

    Added documentation for ChatStrings and CommandExtender

--- src/me/PsionicTemplar/templarIndustries/ChatStrings.java
+++ src/me/PsionicTemplar/templarIndustries/ChatStrings.java
@@ -3,6 +3,13 @@ package me.PsionicTemplar.templarIndustries;
 import net.md_5.bungee.api.ChatColor;
 
 public class ChatStrings {
+	
+	/**
+	 * Returns the error header for a message
+	 * 
+	 * @author Nicholas Braniff
+	 * @return
+	 */
 
 	public static String getError(){
 		return ChatColor.RED + "[" + ChatColor.DARK_RED + "Templar Industries" + ChatColor.RED + "] " + ChatColor.GOLD;
--- src/me/PsionicTemplar/templarIndustries/CommandExtender.java
+++ src/me/PsionicTemplar/templarIndustries/CommandExtender.java
@@ -7,13 +7,21 @@ import org.bukkit.entity.Player;
 
 public class CommandExtender implements CommandExecutor {
 
+	/**
+	 * Switch case for each command. See the code for additional comments.
+	 * 
+	 * @author Nicholas Braniff
+	 */
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command base, String alias, String[] args) {
+		//Set the player if applicable
 		Player p = null;
 		if ((sender instanceof Player)) {
 			p = (Player) sender;
 		}
 		switch(alias.toString()){
+		//Does what I want this command to do. Used in testing.
 		case "test":
 			p.getInventory().addItem(Start.getBlock("Copper Wire").getItemStack());
 			return true;
